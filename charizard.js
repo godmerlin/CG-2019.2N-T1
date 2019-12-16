fcb67fb117edf174bb95f94367bf91735deb9a19
Philipe Soares <31110942+godmerlin@users.noreply.github.com>
2019-12-16 00:13:59 -0300

    Add files via upload

--- charizard.js
+++ charizard.js
@@ -5,11 +5,15 @@ import { GLTFLoader } from './node_modules/three/examples/jsm/loaders/GLTFLoader
 var scene, camera, controls;
 var renderer = new THREE.WebGLRenderer();
 var loader_ground = new THREE.TextureLoader();
-var loader_geodude = new GLTFLoader().setPath( '/modelo/' );
-
+var loader_geodude = new GLTFLoader().setPath( '/modelo/geodude/' );
+var loader_mew = new GLTFLoader().setPath('/modelo/mew/');
+var loader_zapdos = new GLTFLoader().setPath( '/modelo/zapdos/' );
 var ground_y_position = -250;
+var mew;
 var geodude;
+var zapdos;
 var sentido=1;
+var sentidozap=1;
 
 init();
 
@@ -27,7 +31,7 @@ function init(){
 
     // camera
     camera = new THREE.PerspectiveCamera( 40, window.innerWidth / window.innerHeight, 1, 15000 );
-    camera.position.set( 0, 2500, 2500 );
+    camera.position.set( 0, 1000, -2500 );
 
     // luz
     scene.add( new THREE.AmbientLight( 0xaaaaaa ) );
@@ -59,9 +63,9 @@ function init(){
 
     // pokemon
     
-    loader_geodude.load('/scene.gltf',pokemon_load);
+    loader_geodude.load('/scene.gltf',geodude_load);
 
-    function pokemon_load(gltf){
+    function geodude_load(gltf){
         geodude = gltf.scene.children[0];
         geodude.scale.set(1.7,1.7,1.7);
         geodude.position.set(0,ground_y_position+10,0);
@@ -70,30 +74,62 @@ function init(){
         
     }
 
-    // // movimentacao pokemon
-    // function onKeyDown(event) {
-    //     var keyCode = event.which;
-    //     var velocidade = 100;
-    //     if (keyCode == 87) {                   // w
-    //         geodude.position.z -= velocidade; 
-    //     } else if (keyCode == 83) {            // s
-    //         geodude.position.z += velocidade;
-    //     } else if (keyCode == 65) {            // a
-    //         geodude.rotation.z += Math.PI / 32;
-    //     } else if (keyCode == 68) {            // d
-    //         geodude.rotation.z -= Math.PI / 32;;
-    //     } else if (keyCode == 81) {            // q
-    //         if(geodude.position.y >= ground_y_position+200)
-    //             geodude.position.y -= velocidade;
-    //     } else if (keyCode == 69) {            // e
-    //         geodude.position.y += velocidade;
-    //     } else if (keyCode == 32) {            // espaco
-    //         geodude.position.set(50,500,100);
-    //     }
-    // };
-    // document.addEventListener("keydown", onKeyDown, false);
-    
+    loader_mew.load('/scene.gltf',mew_load);
 
+    function mew_load(gltf){
+        mew = gltf.scene.children[0];
+        mew.scale.set(5.7,5.7,5.7);
+        mew.position.set(1000,ground_y_position+500,0);
+        mew.rotation.z = - Math.PI;
+        scene.add(mew);
+    }
+
+    loader_zapdos.load('/scene.gltf',zapdos_load);
+     
+    function zapdos_load(gltf){
+        zapdos = gltf.scene.children[0];
+        zapdos.scale.set(2.7,2.7,2.7);
+        zapdos.position.set(-1000,ground_y_position+500,0);
+        zapdos.rotation.z = - Math.PI;
+        scene.add(zapdos);
+    }
+     // movimentacao pokemon
+     function onKeyDown(event) {
+         var keyCode = event.which;
+        var velocidade = 100;
+         if (keyCode == 87) {                   // w
+             geodude.position.z -= velocidade; 
+         } else if (keyCode == 83) {            // s
+             geodude.position.z += velocidade;
+         } else if (keyCode == 65) {            // a
+             geodude.rotation.z += Math.PI / 32;
+         } else if (keyCode == 68) {            // d
+             geodude.rotation.z -= Math.PI / 32;;
+         } else if (keyCode == 81) {            // q
+            if(geodude.position.y >= ground_y_position+200)
+                geodude.position.y -= velocidade;
+        } else if (keyCode == 69) {            // e
+            geodude.position.y += velocidade;
+         } else if (keyCode == 32) {            // espaco
+             geodude.position.set(50,500,100);
+         }
+     };
+     document.addEventListener("keydown", onKeyDown, false);
+    
+     var listener = new THREE.AudioListener();
+     camera.add( listener );
+     
+     // create a global audio source
+     var sound = new THREE.Audio( listener );
+     
+     // load a sound and set it as the Audio object's buffer
+     var audioLoader = new THREE.AudioLoader();
+     audioLoader.load( 'sounds/audio.ogg', function( buffer ) {
+         sound.setBuffer( buffer );
+         sound.setLoop( true );
+         sound.setVolume( 0.3 );
+         sound.play();
+     });
 }
 
 
@@ -101,14 +137,19 @@ function init(){
 
 
 function moves(){
+        mew.rotation.x += 0.1;
 
+        zapdos.rotation.z += 0.3;
+        zapdos.position.z += (20.0 * sentidozap);
 
-        geodude.position.y += (5.0 * sentido);
+        geodude.position.y += (8.0 * sentido);
 
         if(geodude.position.y<= -250 || geodude.position.y>= 50){
         sentido *=-1;
         } 
-    
+        if(zapdos.position.z <= -1000 || zapdos.position.z>=1000){
+            sentidozap *=-1;
+        }
 }
 
 function animate() {
